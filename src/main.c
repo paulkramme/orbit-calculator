d8606e15403d80a84b061efdf628566de7a72f60
Paul Kramme <pjkramme@gmail.com>
2016-06-19 21:26:52 +0200

    Fix memory error

--- src/main.c
+++ src/main.c
@@ -4,5 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+     if(argc != 5)
+     {
+          printf("USAGE: %s arg1 arg2 arg3 arg4\n", argv[0]);
+     }
+     else
+     {
+          orbit(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+     }
      return 0;
 }
--- src/orbit.h
+++ src/orbit.h
@@ -5,7 +5,7 @@
 
 int orbit(int sat_mass, int obj_mass, int obj_radius, int height)
 {
-     return 0;
+     return sat_mass + obj_mass + obj_radius + height;
 }
 
 #endif
