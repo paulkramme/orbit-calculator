397882eb738d56fc5f0a96d2d04e83381b791afe
Paul Kramme <pjkramme@gmail.com>
2016-06-22 20:50:07 +0200

    Add input system

--- src/main.c
+++ src/main.c
@@ -1,16 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"orbit.h"
+#include<string.h>
 
 int main(int argc, char *argv[])
 {
-     if(argc != 3)
+     if(argc != 2)
      {
-          printf("USAGE: %s orbital_radius mcentral\n", argv[0]);
+          printf("USAGE: %s type type-specific-args\n", argv[0]);
      }
      else
      {
-          orbital_speed(atoi(argv[1]), atoi(argv[2]));
+          if(strcmp(argv[1], "version"))
+          {
+               printf("HELLOWORLD!\n");
+          }
+          else if(strcmp(argv[1], "speed"))
+          {
+               orbital_speed(atoi(argv[1]), atoi(argv[2]));
+          }
+          else if(strcmp(argv[1], "whatever"))
+          {
+               printf("WHATEVER!");
+          }
+          else
+          {
+               printf("USAGE LIST");
+               return 1;
+          }
      }
      return 0;
 }
